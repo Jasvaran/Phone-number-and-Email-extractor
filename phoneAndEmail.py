61a16e313951ec4edd1e615d5181920aaab6e4cd
Jasvaran Mann <mannjesse355@gmail.com>
2022-05-08 16:39:11 -0700

    added email regex

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -4,15 +4,21 @@
 import pyperclip, re
 
 phoneRegex = re.compile(r'''(
-    (\d{3}|\(\d{3}\))?                 #area code
-    (\s|-|\.)?                         #separator
-    (\d{3})                            #first 3 digits
-    (\s|-|\.)                          #separator
-    (\d{4})                            #last 4 digits
-    (\s*(ext|x|ext.)\s*(\d{2,5}))?     #extention
+    (\d{3}|\(\d{3}\))?                 # area code
+    (\s|-|\.)?                         # separator
+    (\d{3})                            # first 3 digits
+    (\s|-|\.)                          # separator
+    (\d{4})                            # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?     # extention
 )''', re.VERBOSE)
 
-# TODO: Create email regex
+# Creat email Regex
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+                  # username
+    @                                  # @ sign
+    [a-zA-Z0-9.-]+                     # domain name
+    (\.[a-zA-Z]{2,4})                  # dot-something
+)''', re.VERBOSE)
 
 
 # TODO: Find matches in clipboard text
